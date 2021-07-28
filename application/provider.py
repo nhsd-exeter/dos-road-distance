e7c105cc18fa0666602cb9c99559d77f7e872b6b
Jay Bush <jay.bush2@nhs.net>
2021-07-28 16:16:56 +0100

    PD-4574 Ccs request handling * Added validation checks for requests and responses * Reorder the schema parameter to last to   be more standard. Comments and removed debugs * Added run-unit-test and help text * Updated import of provider * Added contract test target and generic on unit tests ones * Removed shortcut and extra debug print * Added application folder gitignore * Remove application folder * Rename main folder to application * Changed paths in code for new folder * Code smell updates * Fixes for unit test code smells

---
+++ application/provider.py
@@ -0,0 +1,46 @@
+import os
+import json
+import uuid
+import logging
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError, SchemaError
+from google.protobuf.json_format import Parse
+
+
+class Provider:
+
+    request: dict = {}
+    response: dict = {}
+    options: dict = {}
+    url: str = ""
+    request_id: str = ""
+    contracts_path: str = "openapi_schemas/json/"
+    contracts: dict = {
+        "provider": "travel_time_api",
+        "local": "dos_road_distance_api",
+        "provider-response": "travel_time_api_response",
+    }
+
+    def __init__(self, request):
+        print(os.getcwd())
+        self.request = request
+        self.create_request_id()
+
+    def create_request_id(self):
+        self.request_id = uuid.uuid4()
+
+    def validate_against_schema(self, json: dict, schema_name: str) -> bool:
+        try:
+            contract = self.fetch_json(self.contracts[schema_name] + ".json")
+            validate(instance=json, schema=contract)
+            return True
+        except (ValidationError, SchemaError, Exception) as error:
+            print(error)
+            return False
+
+    def fetch_json(self, file_name: str):
+        try:
+            with open(self.contracts_path + file_name) as json_file:
+                return json.load(json_file)
+        except Exception as ex:
+            print("Exception: Unable to open file " + self.contracts_path + file_name + ". {0}".format(ex))
---
+++ application/tests/unit/test_contracts.py
@@ -0,0 +1,46 @@
+import os
+import json
+import pytest
+from application.provider import Provider
+
+
+class TestContracts:
+
+    """
+    contracts are defined in openapi3 specification
+    and validated via jsonschema
+    """
+
+    provider = Provider({})
+    json_path: str = "tests/unit/test_json/"
+
+    def test_css_to_lambda_contract_happy(self):
+        json_content = self.fetch_json("dos_road_distance_api_happy.json")
+        assert self.provider.validate_against_schema(json_content, "local") == True
+
+    def test_lambda_to_provider_contract_happy(self):
+        json_content = self.fetch_json(self.provider.contracts["provider"] + "_happy.json")
+        assert self.provider.validate_against_schema(json_content, "provider") == True
+
+    def test_provider_to_lambda_contract_happy(self):
+        json_content = self.fetch_json(self.provider.contracts["provider"] + "_response_happy.json")
+        assert self.provider.validate_against_schema(json_content, "provider-response") == True
+
+    def test_contract_css_to_lambda_bad_request(self):
+        json_content = self.fetch_json("dos_road_distance_api_invalid_missing_element.json")
+        assert self.provider.validate_against_schema(json_content, "local") == False
+
+    def test_contract_lambda_to_provider_bad_request(self):
+        json_content = self.fetch_json(self.provider.contracts["provider"] + "_invalid_missing_element.json")
+        assert self.provider.validate_against_schema(json_content, "provider") == False
+
+    def test_contract_lambda_to_provider_bad_response(self):
+        json_content = self.fetch_json(self.provider.contracts["provider"] + "_response_invalid_missing_element.json")
+        assert self.provider.validate_against_schema(json_content, "provider-response") == False
+
+    def fetch_json(self, file_name: str):
+        try:
+            with open(self.json_path + file_name) as json_file:
+                return json.load(json_file)
+        except Exception as ex:
+            print("Exception: Unable to open file " + file_name + ". {0}".format(ex))
