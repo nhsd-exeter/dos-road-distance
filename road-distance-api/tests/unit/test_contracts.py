da7d3348c53137ef50a135f14c18c7d89444cbfb
Jay Bush <jay.bush2@nhs.net>
2021-07-28 16:16:56 +0100

    PD-4574 Initial ccs request handling

--- road-distance-api/tests/unit/test_contracts.py
+++ road-distance-api/tests/unit/test_contracts.py
@@ -1,8 +1,9 @@
 import sys
+sys.path.append("../..")
 import os
 import json
 import pytest
-import provider
+from provider import Provider
 
 class TestContracts:
 
@@ -11,29 +12,29 @@ class TestContracts:
     and validated via jsonschema
     """
 
-    provider = provider()
+    provider = Provider({})
     jsonPath:str = 'test_json/'
 
     def test_css_to_lambda_contract_happy(self):
-        assert provider.validateJson('dos_road_distance_api', self.fetch_json('dos_road_distance_api_happy.json'))
+        assert self.provider.validate_against_schema('dos_road_distance_api', self.fetch_json('dos_road_distance_api_happy.json'))
 
-    def test_lambda_to_provider_contract_happy(self):
-        assert provider.validateJson(provider.contract_name, self.fetch_json(provider.contract_name + '_happy.json'))
+    def test_lambda_to_Provider_contract_happy(self):
+        assert self.provider.validate_against_schema(self.provider.contract_name, self.fetch_json(self.provider.contract_name + '_happy.json'))
 
-    def test_provider_to_lambda_contract_happy(self):
-        assert provider.validateJson(provider.contract_name, self.fetch_json(provider.contract_name + '_response_happy.json'))
+    def test_Provider_to_lambda_contract_happy(self):
+        assert self.provider.validate_against_schema(self.provider.contract_name, self.fetch_json(self.provider.contract_name + '_response_happy.json'))
 
     def test_contract_css_to_lambda_bad_request(self):
         json_content = self.fetch_json('dos_road_distance_api_invalid_missing_element.json')
-        assert provider.validateJson('dos_road_distance_api', json_content) == False
+        assert self.provider.validate_against_schema('dos_road_distance_api', json_content) == False
 
-    def test_contract_lambda_to_provider_bad_request(self):
-        json_content = self.fetch_json(provider.contract_name + '_invalid_missing_element.json')
-        assert provider.validateJson(provider.contract_name, json_content) == False
+    def test_contract_lambda_to_Provider_bad_request(self):
+        json_content = self.fetch_json(Provider.contract_name + '_invalid_missing_element.json')
+        assert self.provider.validate_against_schema(self.provider.contract_name, json_content) == False
 
-    def test_contract_lambda_to_provider_bad_response(self):
-        json_content = self.fetch_json(provider.contract_name + '_response_invalid_missing_element.json')
-        assert provider.validateJson(provider.contract_name, json_content) == False
+    def test_contract_lambda_to_Provider_bad_response(self):
+        json_content = self.fetch_json(Provider.contract_name + '_response_invalid_missing_element.json')
+        assert self.provider.validate_against_schema(self.provider.contract_name, json_content) == False
 
     def fetch_json(self, file_name: str):
         try:
