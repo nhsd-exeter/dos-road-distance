3ccdf42a5cb4577aa26a9d3a82542f2c47e79c94
NM <nicholas.miles2@nhs.net>
2021-08-06 07:52:45 +0100

    PD-4577 Logger code started
    PD-4577 Added rough docker outline to containerise the app
    
    PD-4577 Fix provider logger import and init
    
    PD-4577 Fix provider and tidyup
    
    PD-4577 Updated all of the logger tests
    
    PD-4577 Added makefile target for running logging tests
    
    PD-4577 Add file and console logging
    
    PD-4577 Fixed basic log format
    Added purge and read functions into logger
    Fixed regex
    
    PD-4577 Enabled all tests and fixed the basics
    
    PD-4577 All tests passing
    Added new error states
    Modified some of the regex
    Cleaned up and added sensible fail state output
    
    PD-4577 Some comment changes and additional examples
    
    PD-4577 Added real request and transaction ids
    Added validation of ids
    Removed fake date components and updated validation
    Split up part of the regex for ids
    Added extra assertion test for error logging
    
    PD-4577 Added regex for no transaction id
    
    PD-4577 Removed unused config file
    
    PD-4577 Numeric regex logging tests
    
    PD-4577 Fixed provider code
    Added test stubs for content tests
    
    PD-4577 Removed unused imports
    
    PD-4577 All content tests added
    
    PD-4577 Keep logging folder alive

--- application/tests/unit/test_logging.py
+++ application/tests/unit/test_logging.py
@@ -1,6 +1,7 @@
-from application.provider import Provider
-import pytest
 import re
+import uuid
+import json
+from application.rdlogger import RDLogger
 
 
 class TestLogging:
@@ -18,40 +19,154 @@ class TestLogging:
     YYYY/MM/DD 00:00:00.000000+0100|<info|debug|error>|lambda|<request_id>|<transaction_id>|roaddistancepilot|<request|response>|<success|fail>|<error=|message=>|<additional>
     """
 
-    LOG_DATETIME = "(20[234]\d\/[01]\d\/[0123]\d \d{2}:\d{2}:\d{2}\.\d{6}\+\d{4})"
-    LOG_INFO_PREFIX = "\|info\|lambda"
-    LOG_DETAILS_STATUS = "\|([^|]+)\|([^|]+)\|roaddistancepilot\|system\|success\|message=([^|]*)"
-    LOG_DETAILS_RAW = "\|([^|]+)\|roaddistancepilot\|(ccsrequest|providerrequest|providerresponse)|.*"
-    LOG_FAILURE_PREFIX = "\|error\|lambda\|([^|]+)\|([^|]+)\|roaddistancepilot"
-    LOG_CCS_FAILURE = "\|ccsrequest\|failed\|error=([^|]*)"
-    LOG_PROVIDER_FAILURE = "\|providerresponse\|failed\|error=([^|]*)"
-    LOG_PROVIDER_RESPONSE = "\|([^|]+)\|([^|]+)\|roaddistancepilot\|providerresponse\|success\|reference=([^|]*)\|unreachable=(yes|no)\|distance=([\d.]+)?"
+    LOG1_DATETIME = r"(20[234]\d\/[01]\d\/[0123]\d \d{2}:\d{2}:\d{2}\.(\d{6}\+\d{4}|\d{3}))"
+    LOG2_INFO_PREFIX = r"\|INFO\|lambda"
+    LOG2_FAILURE_PREFIX = r"\|ERROR\|lambda"
+    LOG3_SECOND_PREFIX = r"\|([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})\|([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}|)\|roaddistancepilot"
+    LOG4_DETAILS_BASIC = r"\|([^\|]+)"
+    LOG4_DETAILS_STATUS = r"\|system\|success\|message=([^\|]*)"
+    LOG4_DETAILS_RAW = r"\|(ccsrequest|providerrequest|providerresponse)\|.*"
+    LOG4_CCS_FAILURE = r"\|ccsrequest\|failed\|statuscode=([^\|]*)|error=([^\|]*)"
+    LOG4_PROVIDER_FAILURE = r"\|providerresponse\|failed\|statuscode=([^\|]*)|error=([^\|]*)"
+    LOG4_PROVIDER_RESPONSE = (
+        r"\|providerresponse\|success\|reference=([^\|]*)\|unreachable=(yes|no)\|distance=([\d.]+)?"
+    )
 
-    provider = provider()
+    TEST_PAYLOAD = "This is a test payload/message"
+
+    test_log_path: str = "./tests/unit/test_log/"
+    test_log_file: str = "rd.log"
+    json_path: str = "tests/unit/test_json/"
+    request_id: str = str(uuid.uuid4())
+    transaction_id: str = str(uuid.uuid4())
+    rdlogger = RDLogger(test_log_path + test_log_file, request_id, transaction_id)
+
+    def __fetch_json(self, file_name: str):
+        try:
+            with open(self.json_path + file_name) as json_file:
+                return str(json.load(json_file))
+        except Exception as ex:
+            print("Exception: Unable to open file " + file_name + ". {0}".format(ex))
+
+    def test_basic_log_success(self):
+        rx = self.LOG1_DATETIME + self.LOG2_INFO_PREFIX + self.LOG3_SECOND_PREFIX + self.LOG4_DETAILS_BASIC
+        self.rdlogger.purge()
+        self.rdlogger.log(self.TEST_PAYLOAD)
+        result = re.search(rx, self.rdlogger.read_log_output())
+        print(rx)
+        print(result)
+        assert result is not None
+        rx = self.LOG1_DATETIME + self.LOG2_FAILURE_PREFIX + self.LOG3_SECOND_PREFIX + self.LOG4_DETAILS_BASIC
+        self.rdlogger.purge()
+        self.rdlogger.log(self.TEST_PAYLOAD, "error")
+        result = re.search(rx, self.rdlogger.read_log_output())
+        print(rx)
+        print(result)
+        assert result is not None
 
     def test_status_log_success(self):
-        rx = LOG_DATETIME + LOG_INFO_PREFIX + LOG_DETAILS_STATUS
-        result = provider.format_status_log("This is a message")
-        assert re.search(rx, result) != None
+        rx = self.LOG1_DATETIME + self.LOG2_INFO_PREFIX + self.LOG3_SECOND_PREFIX + self.LOG4_DETAILS_STATUS
+        self.rdlogger.purge()
+        self.rdlogger.log_formatted(self.TEST_PAYLOAD, "status")
+        result = re.search(rx, self.rdlogger.read_log_output())
+        print(rx)
+        print(result)
+        assert result is not None
 
     def test_raw_log_success(self):
-        rx = LOG_DATETIME + LOG_INFO_PREFIX + LOG_DETAILS_RAW
-        result = provider.format_raw_log("This is a message")
-        assert re.search(rx, result) != None
+        rx = self.LOG1_DATETIME + self.LOG2_INFO_PREFIX + self.LOG3_SECOND_PREFIX + self.LOG4_DETAILS_RAW
+        self.rdlogger.purge()
+        self.rdlogger.log_formatted(self.TEST_PAYLOAD, "ccs_request")
+        result = re.search(rx, self.rdlogger.read_log_output())
+        assert result is not None
+        self.rdlogger.purge()
+        self.rdlogger.log_formatted(self.TEST_PAYLOAD, "provider_request")
+        result = re.search(rx, self.rdlogger.read_log_output())
+        assert result is not None
+        self.rdlogger.purge()
+        self.rdlogger.log_formatted(self.TEST_PAYLOAD, "provider_response")
+        result = re.search(rx, self.rdlogger.read_log_output())
+        print(rx)
+        print(result)
+        assert result is not None
 
     def test_provider_response_success_per_destination(self):
-        rx = LOG_DATETIME + LOG_INFO_PREFIX + LOG_PROVIDER_RESPONSE
-        result = provider.format_provider_success_log("1000001", True, 1000)
-        assert re.search(rx, result) != None
-        result = provider.format_provider_success_log("1000001", False)
-        assert re.search(rx, result) != None
+        rx = self.LOG1_DATETIME + self.LOG2_INFO_PREFIX + self.LOG3_SECOND_PREFIX + self.LOG4_PROVIDER_RESPONSE
+        self.rdlogger.purge()
+        self.rdlogger.log_provider_success("1000001", "yes", "1000")
+        result = re.search(rx, self.rdlogger.read_log_output())
+        print(rx)
+        print(result)
+        assert result is not None
+        self.rdlogger.purge()
+        self.rdlogger.log_provider_success("1000001", "no")
+        result = re.search(rx, self.rdlogger.read_log_output())
+        print(rx)
+        print(result)
+        assert result is not None
 
     def test_provider_response_failure(self):
-        rx = LOG_DATETIME + LOG_FAILURE_PREFIX + LOG_PROVIDER_FAILURE
-        result = provider.format_provider_error_log("status_code=422|description=there was an error")
-        assert re.search(rx, result) != None
+        rx = self.LOG1_DATETIME + self.LOG2_FAILURE_PREFIX + self.LOG3_SECOND_PREFIX + self.LOG4_PROVIDER_FAILURE
+        self.rdlogger.purge()
+        self.rdlogger.log_provider_error("422", "there was an error")
+        result = re.search(rx, self.rdlogger.read_log_output())
+        print(rx)
+        print(result)
+        assert result is not None
 
-    def test_ccs_response_failure(self):
-        rx = LOG_DATETIME + LOG_FAILURE_PREFIX + LOG_CCS_FAILURE
-        result = provider.format_ccs_error_log("status_code=422|description=there was an error")
-        assert re.search(rx, result) != None
+    def test_ccs_request_failure(self):
+        rx = self.LOG1_DATETIME + self.LOG2_FAILURE_PREFIX + self.LOG3_SECOND_PREFIX + self.LOG4_CCS_FAILURE
+        self.rdlogger.purge()
+        self.rdlogger.log_ccs_error("422", "there was an error")
+        result = re.search(rx, self.rdlogger.read_log_output())
+        print(rx)
+        print(result)
+        assert result is not None
+
+    def test_content_raw_ccs_request(self):
+        json_content = self.__fetch_json("dos_road_distance_api_happy.json")
+        print(json_content)
+        self.rdlogger.purge()
+        self.rdlogger.log_formatted(json_content, "ccs_request")
+        compare = "ccsrequest|" + json_content
+        result = self.rdlogger.read_log_output().find(compare)
+        print(result)
+        assert result is not -1
+
+    def test_content_raw_provider_request(self):
+        json_content = self.__fetch_json("travel_time_api_happy.json")
+        print(json_content)
+        self.rdlogger.purge()
+        self.rdlogger.log_formatted(json_content, "provider_request")
+        compare = "providerrequest|" + json_content
+        result = self.rdlogger.read_log_output().find(compare)
+        print(result)
+        assert result is not -1
+
+    def test_content_raw_provider_response(self):
+        json_content = self.__fetch_json("travel_time_api_response_happy.json")
+        print(json_content)
+        self.rdlogger.purge()
+        self.rdlogger.log_formatted(json_content, "provider_response")
+        compare = "providerresponse|" + json_content
+        result = self.rdlogger.read_log_output().find(compare)
+        print(result)
+        assert result is not -1
+
+    def test_content_provider_response_success_per_destination(self):
+        self.rdlogger.purge()
+        self.rdlogger.log_provider_success("1000001", "yes", "1000")
+        compare = "providerresponse|success|reference=1000001|unreachable=yes|distance=1000"
+        result = self.rdlogger.read_log_output().find(compare)
+        print(result)
+        assert result is not -1
+
+    def test_content_provider_failure(self):
+        json_content = self.__fetch_json("travel_time_api_error_500.json")
+        print(json_content)
+        self.rdlogger.purge()
+        self.rdlogger.log_provider_error("500", json_content)
+        compare = "providerresponse|failed|statuscode=500|error=" + json_content
+        result = self.rdlogger.read_log_output().find(compare)
+        print(result)
+        assert result is not -1
