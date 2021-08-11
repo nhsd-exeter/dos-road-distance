9e44edb9e82f8d23b2dc32bdff370be43e44ca8d
Jay Bush <jay.bush2@nhs.net>
2021-08-11 16:07:04 +0100

    PD-4573 Added log file purge in handler tests

--- application/tests/unit/test_handler.py
+++ application/tests/unit/test_handler.py
@@ -4,20 +4,27 @@ import application.handler as handler
 
 class TestHandler:
 
+    log_path: str = "tests/unit/test_log/rd.log"
     json_path: str = "tests/unit/test_json/"
     os.environ["LOGGER"] = "Test"
 
     def test_valid_ccs_request(self):
+        self.purge_test_logs()
         request = self.fetch_json("dos_road_distance_api_happy.json")
         response = handler.process_road_distance_request(request, None)
         assert(response['status_code'] == 200)
-        assert(os.path.isfile('tests/unit/test_log/rd.log'))
+        assert(os.path.isfile(self.log_path))
 
     def test_invalid_ccs_request(self):
+        self.purge_test_logs()
         request = self.fetch_json("dos_road_distance_api_invalid_missing_element.json")
         response = handler.process_road_distance_request(request, None)
         assert(response['status_code'] == 500)
-        assert(os.path.isfile('tests/unit/test_log/rd.log'))
+        assert(os.path.isfile(self.log_path))
+
+    def purge_test_logs(self):
+        if os.path.isfile(self.log_path):
+            os.remove(self.log_path)
 
     def fetch_json(self, file_name: str) -> str:
         try:
