dd8d94bc04e2c108eb3c6bfdf0c676256f4c967a
Jay Bush <jay.bush2@nhs.net>
2021-08-12 10:20:42 +0100

    PD-4573 Updated unit tests for handler change

--- application/handler.py
+++ application/handler.py
@@ -6,7 +6,7 @@ def process_road_distance_request(event, context):
     try:
         road_distance = RoadDistance(json.loads(event))
         status_code = road_distance.process_request()
-    except Exception as ex:
+    except Exception:
         status_code = 500
 
     response = {
--- application/tests/unit/test_handler.py
+++ application/tests/unit/test_handler.py
@@ -14,14 +14,14 @@ class TestHandler:
         self.purge_test_logs()
         request = self.fetch_json("dos_road_distance_api_happy.json")
         response = handler.process_road_distance_request(request, None)
-        assert response["status_code"] == 200
+        assert response["statusCode"] == 200
         assert os.path.isfile(self.log_path)
 
     def test_invalid_ccs_request(self):
         self.purge_test_logs()
         request = self.fetch_json("dos_road_distance_api_invalid_missing_element.json")
         response = handler.process_road_distance_request(request, None)
-        assert response["status_code"] == 500
+        assert response["statusCode"] == 500
         assert os.path.isfile(self.log_path)
 
     def test_invalid_log_name_raises_value_error(self):
@@ -29,7 +29,7 @@ class TestHandler:
         os.environ["LOGGER"] = "DoesNotExist"
         request = self.fetch_json("dos_road_distance_api_invalid_missing_element.json")
         response = handler.process_road_distance_request(request, None)
-        assert response["status_code"] == 500
+        assert response["statusCode"] == 500
         assert os.path.isfile(self.log_path) == False
 
     def purge_test_logs(self):
