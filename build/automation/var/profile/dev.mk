619c6209d0872a392e9884bdce299ec330fb75e0
Declan <declan.heard1@nhs.net>
2021-09-20 15:05:24 +0100

    Refactor image version

--- build/automation/var/profile/dev.mk
+++ build/automation/var/profile/dev.mk
@@ -13,4 +13,7 @@ AWS_LAMBDA_ECR = $(or $(AWS_ACCOUNT_ID), 000000000000).dkr.ecr.$(AWS_DEFAULT_REG
 
 STACKS := secrets,lambda,api-gateway
 
+TF_VAR_image_version := $(or $(BUILD_TAG), latest)
 TF_VAR_lambda_tf_state_key := $(PROJECT_ID)/$(ENVIRONMENT)/lambda/terraform.state
+TF_VAR_s3_tf_state_key := $(PROJECT_ID)/$(ENVIRONMENT)/s3/terraform.state
+TF_VAR_codebuild_tf_state_key := $(PROJECT_ID)/$(ENVIRONMENT)/codebuild/terraform.state
