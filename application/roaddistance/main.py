ba563339f7c9705c0f13920fbc4b7534f869641b
Nick Miles <nicholas.miles2@nhs.net>
2021-10-06 15:55:50 +0100

    PD-0 Mock logging improvements (#40)
    * PD-0 Reworked mock configurations for mounting
    
    PD-0 Added mock to make and some better cleanups
    No longer need gitkeep on log as we create it
    
    PD-0 Added conversion of body from event
    
    PD-0 Debug code for DoS request
    
    PD-0 Changed timeout to 10 seconds for DoS async test
    
    PD-0 Fixed delay range
    
    PD-0 Mock delays reverted
    
    PD-0 Fixed path to mock folder
    
    PD-0 Debugging in handler removed
    
    * PD-0 Removed custom make copy
    
    * PD-0 Fixed transaction and request id logging

--- application/roaddistance/main.py
+++ application/roaddistance/main.py
@@ -32,23 +32,22 @@ class RoadDistance(Common):
         self.transaction_id = str(self.request["transactionid"]) if "transactionid" in self.request else ""
         self.request_id = str(uuid.uuid4())
         log_name = os.environ.get("LOGGER", "Audit")
+
         self.logger = RDLogger(log_name, self.request_id, self.transaction_id)
+        self.logger.log_formatted(str(self.request), "ccs_request")
 
     def process_request(self) -> int:
         if not self.validate_against_schema(self.request, "local"):
             self.status_code = 500
-            self.logger.log_ccs_error(
-                str(self.status_code),
-                "Validation error", str(self.request)
-            )
+            self.logger.log_ccs_error(str(self.status_code), "Validation error", str(self.request))
             return self.status_code
 
         try:
             self.logger.log_formatted(str(self.request), "ccs_request")
             self.send_request(self.build_request())
-            if 'error' in self.response and self.response['error']:
+            if "error" in self.response and self.response["error"]:
                 self.status_code = 500
-                self.logger.log("Protobuf returned error in request: " + self.response['error'], "error")
+                self.logger.log("Protobuf returned error in request: " + self.response["error"], "error")
             else:
                 self.log_individual_service_responses()
                 self.status_code = 200
@@ -62,34 +61,32 @@ class RoadDistance(Common):
         for i in range(len(self.request["destinations"])):
             distance = self.response["distances"][i]
             traveltime = self.response["travelTimes"][i]
-            if (traveltime == -1):
+            if traveltime == -1:
                 unreachable = "yes"
                 distance = 999
             else:
                 unreachable = "no"
-            self.logger.log_provider_success(
-                str(self.request["destinations"][i]["reference"]),
-                unreachable,
-                distance
-            )
+            self.logger.log_provider_success(str(self.request["destinations"][i]["reference"]), unreachable, distance)
 
     def send_request(self, request: bytes):
         endpoint = os.environ.get("DRD_ENDPOINT")
         basic_auth = os.environ.get("DRD_BASICAUTH")
         mock_mode = os.environ.get("DRD_MOCK_MODE")
-        if (mock_mode == "True"):
+        if mock_mode == "True":
             self.logger.log("MOCK MODE ENABLED")
             r = TravelTimeMock().post(
-                transaction_id=self.transaction_id,
-                service_count=len(self.request["destinations"])
+                transaction_id=self.transaction_id, service_count=len(self.request["destinations"])
             )
             self.logger.log(r.status_message + "; delay added: " + str(r.delay))
         else:
-            r = requests.post(url=endpoint, data=request, headers={
+            r = requests.post(
+                url=endpoint,
+                data=request,
+                headers={
                     "Authorization": basic_auth,
                     "Content-type": "application/octet-stream",
-                    "Accept": "application/octet-stream"
-                }
+                    "Accept": "application/octet-stream",
+                },
             )
         if r.status_code == 200:
             self.status_code = 200
@@ -97,8 +94,7 @@ class RoadDistance(Common):
         else:
             self.status_code = 500
             self.logger.log_ccs_error(
-                str(self.status_code),
-                "Protobuf endpoint error, status code: " + str(r.status_code)
+                str(self.status_code), "Protobuf endpoint error, status code: " + str(r.status_code)
             )
             self.logger.log("Protobuf endpoint error, status code: " + str(r.status_code), "error")
 
