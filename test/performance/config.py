b9c2434eb3c0f47221a8a9e47e542de273d14093
NM <nicholas.miles2@nhs.net>
2022-04-29 09:31:25 +0100

    DSF-87 Add noauth header to performance configuration

--- test/performance/config.py
+++ test/performance/config.py
@@ -1,4 +1,4 @@
 import os
 
-headers = {'content-type': 'application/json', 'x-authorization': os.environ['API_RD_TOKEN']}
+headers = {'content-type': 'application/json', 'x-authorization': os.environ['API_RD_TOKEN'], 'x-noauth': 'True'}
 ccs_prefix = 'mock/requests/'
