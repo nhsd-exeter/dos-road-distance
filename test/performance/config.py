3825c2d558f385ec143f7710f7a64a46732873f7
Jay Bush <jay.bush2@nhs.net>
2022-05-06 11:47:20 +0100

    DSF-166 Updated required perfromance config

--- test/performance/config.py
+++ test/performance/config.py
@@ -1,4 +1,4 @@
 import os
 
-headers = {'content-type': 'application/json', 'x-authorization': os.environ['API_RD_TOKEN'], 'x-noauth': 'True'}
-ccs_prefix = 'mock/requests/'
+headers = {'content-type': 'application/json', 'x-authorization': '', 'x-noauth': 'True'}
+ccs_prefix = '../../application/roaddistance/mock/requests'
