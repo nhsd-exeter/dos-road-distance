5c58f30b2f5f19aa5171946db23cc5965dd6e03b
Jay Bush <jay.bush2@nhs.net>
2022-05-10 11:56:41 +0100

    Update automation scripts to 20220305083544-02c220f-snapshot

--- build/automation/lib/docker.mk
+++ build/automation/lib/docker.mk
@@ -9,16 +9,16 @@ DOCKER_NETWORK = $(PROJECT_GROUP_SHORT)/$(PROJECT_NAME_SHORT)/$(BUILD_ID)
 DOCKER_REGISTRY = $(AWS_ECR)/$(PROJECT_GROUP_SHORT)/$(PROJECT_NAME_SHORT)
 DOCKER_LIBRARY_REGISTRY = nhsd
 
-DOCKER_ALPINE_VERSION = 3.14.2
+DOCKER_ALPINE_VERSION = 3.15.0
 DOCKER_COMPOSER_VERSION = 2.0.13
 DOCKER_CONFIG_LINT_VERSION = v1.6.0
-DOCKER_DIND_VERSION = 20.10.8-dind
-DOCKER_EDITORCONFIG_CHECKER_VERSION = 2.3.5
-DOCKER_ELASTICSEARCH_VERSION = 7.13.0
+DOCKER_DIND_VERSION = 20.10.12-dind
+DOCKER_EDITORCONFIG_CHECKER_VERSION = 2.4.0
+DOCKER_ELASTICSEARCH_VERSION = 7.17.0
 DOCKER_GRADLE_VERSION = 7.0.2-jdk$(JAVA_VERSION)
 DOCKER_LOCALSTACK_VERSION = $(LOCALSTACK_VERSION)
 DOCKER_MAVEN_VERSION = 3.8.1-openjdk-$(JAVA_VERSION)-slim
-DOCKER_NGINX_VERSION = 1.21.0-alpine
+DOCKER_NGINX_VERSION = 1.21.6-alpine
 DOCKER_NODE_VERSION = $(NODE_VERSION)-alpine
 DOCKER_OPENJDK_VERSION = $(JAVA_VERSION)-alpine
 DOCKER_POSTGRES_VERSION = $(POSTGRES_VERSION)-alpine
@@ -837,8 +837,8 @@ docker-image-get-digest: ### Get image digest by matching tag pattern - mandato
 		REPO=$$(make _docker-get-reg)/$(NAME) \
 		TAG=$(or $(VERSION), $(TAG))
 
-docker-image-find-and-version-as: ### Find image based on git commit hash and tag it - mandatory: VERSION|TAG=[new version/tag],NAME=[image name]; optional: COMMIT=[git commit hash, defaults to HEAD]
-	commit=$(or $(COMMIT), master)
+docker-image-find-and-version-as: ### Find image based on git commit hash and tag it - mandatory: VERSION|TAG=[new version/tag],NAME=[image name]; optional: COMMIT=[git commit hash, defaults to main]
+	commit=$(or $(COMMIT), $$(make git-branch-get-main-name))
 	hash=$$(make git-commit-get-hash COMMIT=$$commit)
 	digest=$$(make docker-image-get-digest NAME=$(NAME) TAG=$$hash)
 	make docker-pull NAME=$(NAME) DIGEST=$$digest
