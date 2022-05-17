b68e66f6cc4420afe0c05c01f448cc5ba8dc6781
NM <nicholas.miles2@nhs.net>
2022-05-17 12:10:38 +0100

    DSF-166 Added scenario 1b file

---
+++ test/performance/scenario1b.py
@@ -0,0 +1,68 @@
+import json
+from locust import task, FastHttpUser, tag, between
+import config as config
+
+
+class FiveDest(FastHttpUser):
+    weight = 0
+    wait_time = between(0.5, 2)
+    def on_start(self):
+        with open(config.ccs_prefix + 'ccs_5_destinations.json') as json_file:
+            self.payload = json.load(json_file)
+
+    @tag('load')
+    @task
+    def start_test(self):
+        self.client.post("", data=json.dumps(self.payload), headers=config.headers)
+
+
+class FiftyDest(FastHttpUser):
+    weight = 0
+
+    def on_start(self):
+        with open(config.ccs_prefix + 'ccs_50_destinations.json') as json_file:
+            self.payload = json.load(json_file)
+
+    @tag('load')
+    @task
+    def start_test(self):
+        self.client.post("", data=json.dumps(self.payload), headers=config.headers)
+
+
+class FiveHundredDest(FastHttpUser):
+    weight = 0
+
+    def on_start(self):
+        with open(config.ccs_prefix + 'ccs_500_destinations.json') as json_file:
+            self.payload = json.load(json_file)
+
+    @tag('load')
+    @task
+    def start_test(self):
+        self.client.post("", data=json.dumps(self.payload), headers=config.headers)
+
+
+class OneThousandFiveHundredDest(FastHttpUser):
+    weight = 0
+
+    def on_start(self):
+        with open(config.ccs_prefix + 'ccs_1500_destinations.json') as json_file:
+            self.payload = json.load(json_file)
+
+    @tag('load')
+    @task
+    def start_test(self):
+        self.client.post("", data=json.dumps(self.payload), headers=config.headers)
+
+
+class ThreeThousandDest(FastHttpUser):
+    weight = 100
+
+    def on_start(self):
+        with open(config.ccs_prefix + "ccs_3000_destinations.json") as json_file:
+            self.payload = json.load(json_file)
+
+    @tag('load')
+    @task
+    def start_test(self):
+        self.client.post("", data=json.dumps(self.payload), headers=config.headers)
