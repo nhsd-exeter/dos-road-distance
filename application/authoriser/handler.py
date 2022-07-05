53a53a72ea0f774f51042b1bec674ffb73ab07dd
Christian Martin <christian.martin2@nhs.net>
2022-07-05 11:28:48 +0100

    DSF-200 Remove additional context word

--- application/authoriser/handler.py
+++ application/authoriser/handler.py
@@ -22,11 +22,11 @@ def authorize_api_request(event, context) -> dict:
         print("Authentication method failed with error: {}".format(e))
         response = {
             "isAuthorized": False,
-            "cloudWatchStreamName:": context.context.log_stream_name or '',
-            "cloudWatchLogGroupName:": context.context.log_group_name or '',
-            "lambdaFunctionArn:": context.invoked_function_arn or '',
-            "lambdaRequestId:": context.context.aws_request_id or '',
-            "lambdaMemoryLimit:": context.context.memory_limit_in_mb or ''
+            "cloudWatchStreamName:": context.log_stream_name or "",
+            "cloudWatchLogGroupName:": context.log_group_name or "",
+            "lambdaFunctionArn:": context.invoked_function_arn or "",
+            "lambdaRequestId:": context.aws_request_id or "",
+            "lambdaMemoryLimit:": context.memory_limit_in_mb or "",
         }
     print("Response: {}".format(response))
     return response
