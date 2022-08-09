7523f01f8c2450ebd135a66b5aec1a8f2c45dd4e
Christian Martin <christian.martin2@nhs.net>
2022-08-09 11:26:09 +0100

    DSF-0 Change secrets variable

--- application/authoriser/handler.py
+++ application/authoriser/handler.py
@@ -28,13 +28,13 @@ def authorize_api_request(event, context) -> dict:
 
 def check_authorisation_token(token_hash_sent: str, noauth: bool) -> bool:
     client = boto3.client("secretsmanager")
-    if noauth and os.environ.get("DRD_ALLOW_NO_AUTH_XXX", False):
+    if noauth and os.environ.get("DRD_ALLOW_NO_AUTH", False):
         logger.log_info("Noauth actioned as allowed")
         return True
     if not re.match(r"\$2[aby]", token_hash_sent):
         return False
     secrets_response = client.get_secret_value(
-        SecretId=os.environ["SECRET_STORE"],
+        SecretId=os.environ["SECRET_STORE_XXX"],
     )
     secrets = json.loads(secrets_response["SecretString"])
     time_factor = str(int(time.time() / 1800))
