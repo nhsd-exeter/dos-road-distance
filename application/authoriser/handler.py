7f466d9f542e3e8ac12499f1332b80185eb31326
Christian Martin <christian.martin2@nhs.net>
2022-08-09 11:15:16 +0100

    DSF-0 Change no auth variable

--- application/authoriser/handler.py
+++ application/authoriser/handler.py
@@ -22,18 +22,13 @@ def authorize_api_request(event, context) -> dict:
         else:
             logger.log_error("Authentication failed", "Invalid token hash sent")
     except Exception as ex:
-        logger.log_exception_error(
-            type(ex).__name__,
-            "Authentication method failed with error",
-            ex.args,
-            ex
-        )
+        logger.log_exception_error(type(ex).__name__, "Authentication method failed with error", ex.args, ex)
     return response
 
 
 def check_authorisation_token(token_hash_sent: str, noauth: bool) -> bool:
     client = boto3.client("secretsmanager")
-    if noauth and os.environ.get("DRD_ALLOW_NO_AUTH", False):
+    if noauth and os.environ.get("DRD_ALLOW_NO_AUTH_XXX", False):
         logger.log_info("Noauth actioned as allowed")
         return True
     if not re.match(r"\$2[aby]", token_hash_sent):
