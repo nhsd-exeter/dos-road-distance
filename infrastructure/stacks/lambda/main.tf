87be7613791990cc5aa39bc71e4f23d29a9b6738
Nick Miles <nicholas.miles2@nhs.net>
2023-08-17 15:39:22 +0100

    DS-804 Propagate secret token

--- infrastructure/stacks/lambda/main.tf
+++ infrastructure/stacks/lambda/main.tf
@@ -12,9 +12,7 @@ resource "aws_lambda_function" "road_distance_lambda" {
   }
   environment {
     variables = {
-      "DRD_APP_ID"    = var.drd_app_id
-      "DRD_API_KEY"   = var.drd_api_key
-      "DRD_ENDPOINT"  = var.drd_endpoint
+      "SECRET_STORE"  = var.deployment_secrets
       "DRD_MOCK_MODE" = var.drd_mock
     }
   }
