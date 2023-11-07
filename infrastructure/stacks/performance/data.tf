568adf10378f5afd48dc87645adfb6cf8a527c73
K Odewale <kayode.odewal1@nhs.net>
2023-11-07 08:55:14 +0000

    DS-1675 Updated aws role used by Performance Stack

--- infrastructure/stacks/performance/data.tf
+++ infrastructure/stacks/performance/data.tf
@@ -12,11 +12,3 @@ data "aws_iam_policy_document" "iam_policy" {
   }
 }
 
-data "terraform_remote_state" "eks" {
-  backend = "s3"
-  config = {
-    bucket = var.terraform_platform_state_store
-    key    = var.eks_terraform_state_key
-    region = var.aws_region
-  }
-}
