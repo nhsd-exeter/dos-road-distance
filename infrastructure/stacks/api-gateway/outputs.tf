2051cb03554855acda207bc3d4b3bc7646e368b7
Stuart Westbrook <stuart.westbrook1@nhs.net>
2024-01-11 12:12:06 +0000

    DS-1530 Attempt to deploy in performance with dr naming

--- infrastructure/stacks/api-gateway/outputs.tf
+++ infrastructure/stacks/api-gateway/outputs.tf
@@ -1,9 +1,9 @@
 output "api_execution_arn" {
   description = "Execution ARN value for the API Gateway"
-  value       = aws_apigatewayv2_api.road_distance_apigateway.execution_arn
+  value       = aws_apigatewayv2_api.road_distance_dr_apigateway.execution_arn
 }
 
 output "api_id" {
   description = "ID of the API Gateway"
-  value       = aws_apigatewayv2_api.road_distance_apigateway.id
+  value       = aws_apigatewayv2_api.road_distance_dr_apigateway.id
 }
