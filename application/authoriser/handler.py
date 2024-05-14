7fe8854b3e1f4773a284597e6d68cb4cebe5418d
Neil Olner <nolner@gmail.com>
2024-05-14 16:51:12 +0100

    DS-2346 Add Token Caching

--- application/authoriser/handler.py
+++ application/authoriser/handler.py
@@ -24,18 +24,37 @@ def authorize_api_request(event, context) -> dict:
     return response
 
 def check_authorisation_token(token_hash_sent: str, noauth: bool) -> bool:
-    client = boto3.client("secretsmanager")
     if noauth and os.environ.get("DRD_ALLOW_NO_AUTH", "False") == "True":
         logger.log_info("Noauth actioned as allowed")
         return True
+
+    time_x = str(int((time.time() - 900) / 900))
+    time_y = str(int(time.time() / 900))
+    token_hash_sent_encoded = re.sub(r"^\$2y", "$2b", token_hash_sent).encode("utf-8")
+
+    if os.environ.get("RD_API_TOKEN_X_SLOT", "") == time_x and os.environ.get("RD_API_TOKEN_Y_SLOT", "") == time_y:
+        if os.environ.get("RD_API_TOKEN_X_TOKEN", "") == token_hash_sent or os.environ.get("RD_API_TOKEN_Y_TOKEN", "") == token_hash_sent:
+            return True
+
     if not re.match(r"^\$2[by]\$(0[4-9]|1[012])\$[.\/0-9A-Za-z]{21}[.Oeu][.\/0-9A-Za-z]{31}$", token_hash_sent):
         return False
+
+    client = boto3.client("secretsmanager")
+
     secrets_response = client.get_secret_value(
         SecretId=os.environ["SECRET_STORE"],
     )
     secrets = json.loads(secrets_response["SecretString"])
-    token_x = (str(secrets["ROAD_DISTANCE_API_TOKEN"]) + str(int((time.time() - 900) / 900))).encode("utf-8")
-    token_y = (str(secrets["ROAD_DISTANCE_API_TOKEN"]) + str(int(time.time() / 900))).encode("utf-8")
-    token_hash_sent_encoded = re.sub(r"^\$2y", "$2b", token_hash_sent).encode("utf-8")
-    return bcrypt.checkpw(token_y, token_hash_sent_encoded) or bcrypt.checkpw(token_x, token_hash_sent_encoded)
+    token_x = (str(secrets["ROAD_DISTANCE_API_TOKEN"]) + time_x)
+    token_y = (str(secrets["ROAD_DISTANCE_API_TOKEN"]) + time_y)
+
+    if bcrypt.checkpw(token_y.encode("utf-8"), token_hash_sent_encoded) or bcrypt.checkpw(token_x.encode("utf-8"), token_hash_sent_encoded):
+        os.environ["RD_API_TOKEN_X_SLOT"] = time_x
+        os.environ["RD_API_TOKEN_X_TOKEN"] = token_x
+        os.environ["RD_API_TOKEN_Y_SLOT"] = time_y
+        os.environ["RD_API_TOKEN_Y_TOKEN"] = token_y
+
+        return True
+
+    return False
 
