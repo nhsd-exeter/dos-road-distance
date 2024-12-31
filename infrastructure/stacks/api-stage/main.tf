bc49be1c6162d05edba8ddaf3c14053d0c54a44a
Christian Martin <christian.martin2@nhs.net>
2024-12-31 15:33:57 +0000

    DS-237 Amend rd alias with correct name

--- infrastructure/stacks/api-stage/main.tf
+++ infrastructure/stacks/api-stage/main.tf
@@ -12,9 +12,9 @@ resource "aws_apigatewayv2_stage" "road_distance_api_stage" {
 }
 
 resource "aws_lambda_alias" "latest-rd-lambda-version" {
-  name             = "latest"
+  name             = "latest-rd-lambda-version"
   function_name    = "${var.service_prefix}-rd-lambda"
-  function_version = aws_lambda_function.rd_lambda.version
+  function_version = aws_lambda_function.road_distance_lambda.version
 }
 
 resource "aws_lambda_permission" "road_distance_invoke_lambda_permission" {
