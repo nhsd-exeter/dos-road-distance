9b4f3ea6234fc40b39beb8a2fd951cbbbc9e89a9
Gurinder Brar <gurinder.brar1@nhs.net>
2025-07-28 22:22:59 +0100

    Performance Tests Investigation

--- test/performance/config.py
+++ test/performance/config.py
@@ -1,4 +1,10 @@
 import os
 
+# Configuration for performance testing
+# Set the base host URL - this should be your actual API Gateway URL
+# Format: https://{api-id}.execute-api.{region}.amazonaws.com
+BASE_HOST = os.environ.get('PERF_TEST_HOST', 'https://7bmdvfro69.execute-api.eu-west-2.amazonaws.com')
+API_ENDPOINT = '/'  # Your API Gateway route is POST /
+
 headers = {'content-type': 'application/json', 'x-authorization': '', 'x-noauth': 'True'}
 ccs_prefix = 'mock/requests/'
