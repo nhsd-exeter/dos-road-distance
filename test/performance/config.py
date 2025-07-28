970639737327f68253368e848a144f6b3a76fd61
Gurinder Brar <gurinder.brar1@nhs.net>
2025-07-28 18:21:47 +0100

    Performance Tests Investigation

--- test/performance/config.py
+++ test/performance/config.py
@@ -2,7 +2,7 @@ import os
 
 # Get API endpoint from environment variable (set by Kubernetes ConfigMap)
 # or use a default for local development
-API_ENDPOINT = os.getenv('API_ENDPOINT', 'https://api-nonprod.k8s-nonprod.texasplatform.uk/dosapi/roaddistance/v1/calculate')
+API_ENDPOINT = os.getenv('API_ENDPOINT', 'http://localhost:8080')
 
 headers = {'content-type': 'application/json', 'x-authorization': '', 'x-noauth': 'True'}
-ccs_prefix = 'mock/requests/'
+ccs_prefix = '../../application/roaddistance/mock/requests/'
