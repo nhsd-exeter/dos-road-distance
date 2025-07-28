1ee4aabadbc78e8370a90a9fb3496c7e2848425c
Ankit Kumar <193604438+ankitbvkumar@users.noreply.github.com>
2025-07-28 14:15:06 +0100

    DOSIS-1638 Update Sonar token

--- build/automation/lib/sonar.mk
+++ build/automation/lib/sonar.mk
@@ -3,7 +3,7 @@ SONAR_SCANNER_CLI_VERSION = 4.6
 sonar-scanner-cli: ### Run Sonar scanner CLI- mandatory: SONAR_HOST_TOKEN; optional: SONAR_HOST_URL=[defaults to 'https://sonarcloud.io'],SONAR_EXCLUSIONS=[e.g. '**/*.java'],SONAR_ARGS
 	make docker-run-sonar-scanner-cli CMD=" \
 		-D sonar.host.url='$(or $(SONAR_HOST_URL), https://sonarcloud.io)' \
-		-D sonar.login='$(SONAR_HOST_TOKEN)' \
+		-D sonar.login='$(RD_SONAR_TOKEN)' \
 		-D sonar.organization='$(ORG_NAME)' \
 		-D sonar.projectKey='$(PROJECT_ID)' \
 		-D sonar.projectName='$(PROJECT_DISPLAY_NAME)' \
